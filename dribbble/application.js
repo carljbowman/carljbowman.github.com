f5cd33f3b0dd78eb0657b788c341b9e26fce5d38
carljbowman <carljbowman@gmail.com>
2014-04-14 10:53:43 -0400

    Update to Dribble

--- dribbble/application.js
+++ dribbble/application.js
@@ -7,18 +7,30 @@ function createShot(shot, i) {
       
     var title = $("<div class='title'>").text(shot.title);
     var designer = $("<div class='designer'>").text(shot.player.name);
+    var designerPhoto = $("<img class='designerPhoto'>").attr("src", shot.player.avatar_url);
+    var info = $("<div class='title'>").text(shot.title);
     var views = $("<div class='views'>").text(shot.views_count);
       	var viewsEye = $("<img class='views-image'>").attr("src", 'images/views.png');
       
     
     var image = $("<img class='image'>").attr("src", shot.image_url);
+    var imagelg = $("<img class='imagelg'>").attr("src", shot.image_url);
     
         
      $("#shots").append(shotContainer);
      $(shotContainer).append(image);
      
-     $(shotContainer).click(function() {
-		$("#info").empty().append(title);
+     $(shotContainer).mouseenter(function() {
+		$("#info").empty().fadeIn(500).append(designerPhoto, designer, title, views, viewsEye);
+		
+	}); 
+	
+	$(shotContainer).click(function() {
+		$("#overlay").empty().slideDown(1000).append(imagelg);
+	}); 
+	
+	$("#overlay").click(function() {
+		$("#overlay").slideUp(1000);
 	}); 
 	 
 };
