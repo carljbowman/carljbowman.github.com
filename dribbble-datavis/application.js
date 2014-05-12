f43f7d2b43ade7e005c7df40f9c77ba1d05abc3d
carljbowman <carljbowman@gmail.com>
2014-05-12 00:39:03 -0400

    Updates to Styling Dribble DataVis

--- dribbble-datavis/application.js
+++ dribbble-datavis/application.js
@@ -3,6 +3,31 @@
 
 // When the page loads, add in our event handlers
 $(function() {
+
+    $('*[data-type-id="helvetica"').delay(500).queue(function(){
+    													$(this).addClass("helvetica");
+    													toggleOn("helvetica");
+    													});
+    													
+    $('*[data-type-id="futura"').delay(1000).queue(function(){
+    													$(this).addClass("futura");
+    													toggleOn("futura");
+    													});
+    													
+    $('*[data-type-id="knockout"').delay(1500).queue(function(){
+    													$(this).addClass("knockout");
+    													toggleOn("knockout");
+    													});
+    													
+    $('*[data-type-id="din"').delay(2000).queue(function(){
+    													$(this).addClass("din");
+    													toggleOn("din");
+    													});												
+    $('*[data-type-id="opensans"').delay(2500).queue(function(){
+    													$(this).addClass("opensans");
+    													toggleOn("opensans");
+    													});																
+    
     
    	$(".toggle").click(function() {
 		var key = $(this).data("type-id");
